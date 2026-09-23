862cfdbcf09419df13896ec20583df13765abdcb
Lovelly <lovellyinvitation@gmail.com>
2026-09-23 13:21:32 +0700

    oke

--- app/src/main/java/com/example/relaxbro/MainActivity.java
+++ app/src/main/java/com/example/relaxbro/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.relaxbro;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
@@ -24,13 +25,35 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
-        // 1. Inisialisasi Pop It Toy Button
+        // 1. Inisialisasi button pindah activity
         ImageView btnPopIt = findViewById(R.id.btnPopIt);
-
+        Button btnPindah = findViewById(R.id.btnPindah);
         // 2. Klik Pop It Toy Button untuk berpindah ke PopItActivity
         btnPopIt.setOnClickListener(v -> {
             Intent intent = new Intent(MainActivity.this, PopItActivity.class);
             startActivity(intent);
         });
+        btnPindah.setOnClickListener(v -> {
+            // 3. Intent(Activity_Saat_Ini.this, Activity_Tujuan.class)
+            Intent intent = new Intent(MainActivity.this, PianoActivity.class);
+            startActivity(intent);
+        });
+
+        // 3. Inisialisasi Piano / Fidget 2 Button
+        ImageView btnFidget2 = findViewById(R.id.btnFidget2);
+        if (btnFidget2 != null) {
+            btnFidget2.setOnClickListener(v -> {
+                Intent intent = new Intent(MainActivity.this, PianoActivity.class);
+                startActivity(intent);
+            });
+        }
+
+        ImageView btnFidget3 = findViewById(R.id.btnFidget3);
+        if (btnFidget3 != null) {
+            btnFidget3.setOnClickListener(v -> {
+                Intent intent = new Intent(MainActivity.this, PianoActivity.class);
+                startActivity(intent);
+            });
+        }
     }
 }
